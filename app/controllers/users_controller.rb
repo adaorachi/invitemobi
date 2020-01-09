7b11c5bc64ce7f9b17139f9ef77f98265be48ffd
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-01-09 23:46:42 +0100

    adjust user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, except: [:new, :create]
-  before_action :already_logged_in, only: [:new, :create]
+  # before_action :logged_in_user, except: [:new, :create]
+  # before_action :already_logged_in, only: [:new, :create]
 
   def index
     @users = User.all
   end
 
   def show
-    @events_all = Event.where('creator_id = ?', current_user)
-    event_type = params[:type]
-    if event_type == 'past_events'
-      @user_events = Event.past(current_user)
-      @user_invited_events = Event.invited_past(current_user)
-      @event_text = 'past'
-    elsif !event_type || event_type == 'upcoming_events'
-      @user_events = Event.upcoming(current_user)
-      @user_invited_events = Event.invited_upcoming(current_user)
-      @event_text = 'upcoming'
-    end
+    # @events_all = Event.where('creator_id = ?', current_user)
+    # event_type = params[:type]
+    # if event_type == 'past_events'
+    #   @user_events = Event.past(current_user)
+    #   @user_invited_events = Event.invited_past(current_user)
+    #   @event_text = 'past'
+    # elsif !event_type || event_type == 'upcoming_events'
+    #   @user_events = Event.upcoming(current_user)
+    #   @user_invited_events = Event.invited_upcoming(current_user)
+    #   @event_text = 'upcoming'
+    # end
   end
 
   def new
